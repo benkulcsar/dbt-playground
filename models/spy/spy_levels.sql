809bdbae7287b8c2ffe30e6ab1d84db4bccfcb9d
Ben Kulcsar <b.l.kulcsar@gmail.com>
2022-08-20 10:50:48 +0000

    Post hook

--- models/spy/spy_levels.sql
+++ models/spy/spy_levels.sql
@@ -1,3 +1,10 @@
+{{ config(
+    post_hook=[
+        "create table if not exists public.runcounter (spy_runs INTEGER)",
+        "insert into public.runcounter values (1)",
+    ]
+) }}
+
 select 
     spy, 
     skill, 
